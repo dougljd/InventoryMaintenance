d128c124fb58934413002c72a4d913135dbb6b0b
jddoug2005 <jddoug2005@gmail.com>
2026-09-17 13:36:51 -0400

    Add Plant class with Size property and display override
    Added Plant class to InventoryMaintenance, inheriting from InvItem. Implemented Size property, constructor with size, and GetDisplayText override to include size in output.

---
+++ InventoryMaintenance/Plant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    internal class Plant : InvItem
+    {
+        Plant() { }
+
+        public Plant(int itemNo, string description, decimal price, string size): base(itemNo, description, price)
+        {
+            this.Size = size;
+        }
+
+        public string Size { get; set; }
+
+        public override string GetDisplayText() => $"{ItemNo} {Size} {Description} ({Price:c})";
+
+    }
+}
